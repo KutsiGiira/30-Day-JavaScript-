4451f80b6d109c5109c3068c0ddd0b78a0934c02
KutsiGiira <haytamboualkhours@gmail.com>
2025-01-17 22:03:40 +0100

    git init

--- Day_12/index.js
+++
@@ -1,5 +0,0 @@
-// Exercises: Level 1
-// Write a pattern which identify if a string is a valid JavaScript variable
-const isValidVariableName = /^[a-zA-Z_$][a-zA-Z0-9_$]*$/;
-console.log(isValidVariableName.test('KutsiGira'));
-console.log(isValidVariableName.test('2Kutsi'));
